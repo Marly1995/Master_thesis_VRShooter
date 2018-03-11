236560732ef8418ddf641ee21fd50448cb662ac5
Marlon Gilliam <14471455@students.lincoln.ac.uk>
2018-03-11 16:18:55 +0000

    enemy patrol working

---
+++ Assets/Scripts/Enemy/EnemyBehaviours.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviours : MonoBehaviour
+{
+    AgentMove controller;
+
+    public Transform[] patrolPositions;
+
+    int patrolIndex;
+
+    private void Start()
+    {
+        controller = GetComponent<AgentMove>();
+        ChooseNextPatrolPosition();
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(controller.transform.position, controller.goal.position) <= 1f)
+        {
+            ChooseNextPatrolPosition();
+        }
+    }
+
+    void ChooseNextPatrolPosition()
+    {
+        if (patrolIndex < patrolPositions.Length)
+        {
+            controller.goal = patrolPositions[patrolIndex++];
+        }
+        else
+        {
+            patrolIndex = 0;
+            controller.goal = patrolPositions[patrolIndex];
+        }
+    }
+}
