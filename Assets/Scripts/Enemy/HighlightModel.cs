d7aebdcd0e22b6b17c758a6a7ae2f607f2fc41c3
Marlon Gilliam <14471455@students.lincoln.ac.uk>
2018-03-19 15:35:07 +0000

    xray shader works

---
+++ Assets/Scripts/Enemy/HighlightModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightModel : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float highlightIntensity;
+
+    SkinnedMeshRenderer mr;
+
+    private void Start()
+    {
+        mr = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    private void Update()
+    {
+        mr.sharedMaterial.SetFloat("_GlobalXRayVisibility", highlightIntensity);
+    }
+}
